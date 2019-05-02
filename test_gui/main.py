0f07bd98b3fd7b854c5516279cfa0a13bd67ae1f
Shoomaher <mihail.shoomaher@gmail.com>
2019-05-02 22:45:33 +0300

    GUi prototype

--- test_gui/main.py
+++ test_gui/main.py
@@ -1,25 +1,50 @@
 from kivy.app import App
-from kivy.uix.label import Label
 from kivy.uix.gridlayout import GridLayout
 from kivy.uix.textinput import TextInput
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.label import Label
+from kivy.uix.button import Button
 
 
-class LoginScreen(GridLayout):
+class DataPanel(BoxLayout):
+    def __init__(self, **kwargs):
+        super(DataPanel, self).__init__(**kwargs)
+        self.orientation = 'vertical'
+
+        top_text = BoxLayout(orientation='horizontal', padding=30)
+        top_text.add_widget(Label(text='Enter expression:'))
+        top_text.add_widget(Label(text='Enter function edges a and b:'))
+        bottom_fields = BoxLayout(
+            orientation='horizontal', padding=20, spacing=20)
+        bottom_fields.add_widget(Label(text='f(x) = '))
+
+        self.expression = TextInput(multiline=False)
+        self.a_edge = TextInput(multiline=False)
+        self.b_edge = TextInput(multiline=False)
+        self.run_btn = Button(text='RUN')
+
+        bottom_fields.add_widget(self.expression)
+        bottom_fields.add_widget(self.a_edge)
+        bottom_fields.add_widget(self.b_edge)
+        bottom_fields.add_widget(self.run_btn)
+
+        self.add_widget(top_text)
+        self.add_widget(bottom_fields)
+
+
+class MainScreen(BoxLayout):
 
     def __init__(self, **kwargs):
-        super(LoginScreen, self).__init__(**kwargs)
-        self.cols = 2
-        self.add_widget(Label(text='User Name'))
-        self.username = TextInput(multiline=False)
-        self.add_widget(self.username)
-        self.add_widget(Label(text='password'))
-        self.password = TextInput(password=True,  multiline=False)
-        self.add_widget(self.password)
+        super(MainScreen, self).__init__(**kwargs)
+
+        self.orientation = 'vertical'
+        self.data_panel = DataPanel()
+        self.add_widget(self.data_panel)
 
 
 class MainApp(App):
     def build(self):
-        return LoginScreen()
+        return MainScreen()
 
 
 if __name__ == "__main__":
